469cae8bfb8938b661cef9302f177763d240beef
Goutam <goutam.kathuria@gmail.com>
2026-01-30 16:42:26 +0530

    updates

--- src/api/endpoints.jsx
+++ src/api/endpoints.jsx
@@ -1,6 +1,6 @@
 // export const API_BASE_URL = "http://localhost:7860";
-export const API_BASE_URL = "https://india-collage-fest-api.onrender.com/";
-
+// export const API_BASE_URL = "https://india-collage-fest-api.onrender.com/";
+export const API_BASE_URL = "http://172.93.223.239:3001/"
 
 // All API endpoints in one place
 export const ENDPOINTS = {
--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:7860";
+export const BASE_URL = "http://172.93.223.239:3001";
 export const AUTH_EXPIRY_TIME = 24 * 60 * 60 * 1000; // 1 day
