2bdf5ec0cfb4c25aac42f004e9b5eb790305c5f7
Goutam <goutam.kathuria@gmail.com>
2026-02-27 11:38:18 +0530

    updates

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
     <img
       src={logo}
       alt="Site Logo"
-      className="h-auto w-[132px] max-w-full object-contain select-none drop-shadow-[0_8px_18px_rgba(29,78,216,0.18)] transition-transform duration-300 hover:scale-[1.01] sm:w-[150px] md:w-[172px] lg:w-[188px] xl:w-[200px]"
+      className="h-auto w-[160px] min-w-[140px] shrink-0 max-w-full object-contain select-none drop-shadow-[0_8px_18px_rgba(29,78,216,0.18)] transition-transform duration-300 hover:scale-[1.01] sm:w-[178px] sm:min-w-[160px] md:w-[196px] lg:w-[212px] xl:w-[224px]"
       draggable="false"
     />
   );
